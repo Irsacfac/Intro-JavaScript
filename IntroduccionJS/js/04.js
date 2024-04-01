bd3070c266da8add7e5db556e3745e87b15fac8f
Isaac <irsacfac@gmail.com>
2024-03-31 18:28:27 -0600

    Update 04.js

--- IntroduccionJS/js/04.js
+++ IntroduccionJS/js/04.js
@@ -1,11 +1,24 @@
-const producto = "Monitor de 20 Pulgadas"
-const producto2 = String("Monitor 30 pulgadas")
+const producto = "Monitor de 20 Pulgadas";
+const producto2 = String("Monitor HD");
 const producto3 = new String("Monitor 50 pulgadas")
-const producto4 = "Monitor de 20\""
-const producto5 = 'Monitor de 20"'
+const producto4 = "Monitor de 20\""; //para usal el signo de: "
+const producto5 = 'Monitor de 20"'; //otra forma de escribir: "
 
-console.log(producto)
-console.log(producto2)
-console.log(producto3)
-console.log(producto4)
-console.log(producto5)
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo Web Completo";
+
+console.log(tweet.length);
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
+console.log(producto4);
+console.log(producto5);
+
+// IndexOf (retorna una posición)
+console.log(tweet.indexOf("JavaScript")); //encontrar elemento en un string
+console.log(producto2.indexOf("Tablet")); //numero negativo indica que no se encontró
+
+// Include (retorna un boolean)
+
+console.log(tweet.includes("JavaScript"));
+console.log(producto2.includes("Tablet"));
