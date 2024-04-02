93bab83529615b449c4d8460cc6d9f4f18772e55
Isaac <irsacfac@gmail.com>
2024-04-02 00:32:49 -0600

    concatenacion de string y booleans

---
+++ IntroduccionJS/js/08.js
@@ -0,0 +1,11 @@
+const nombre = "Isaac";
+const email = "correo@correo.com";
+
+console.log(nombre);
+console.log(email);
+
+// Concatenacion string
+console.log("Nombre: " +  nombre + " Email: " + email);
+
+// Template string - String literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`) //más utilizada, se evita la concatenación
